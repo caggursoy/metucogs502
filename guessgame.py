9a46dc13d814cebbfbd6f4a8cdd5a6b4e8429fe6
N. Çağatay Gürsoy <caggursoy93@icloud.com>
2017-04-29 01:41:18 +0300

    Update guessgame.py

--- guessgame.py
+++ guessgame.py
@@ -10,27 +10,25 @@ import random
 import time
 
 file = open("boun.list.txt",'r')
-file2 = open("boun.list.txt",'r')
 randomNum = random.randint(0,25000)
 str = ''
 newStr = '' 
 inStr = ''
 ind = 0
 
-
 for line in file:
 	if ind == randomNum:
 		str = line
 	ind += 1
 str = str[:-1]
-newStr = "".join(random.sample(str, len(str)))
+newStr = "".join(random.sample(str, len(str))) # Shuffle the word
 # print(str) # for debugging purposes, if you want to play the game fair and square do not uncomment this line
-print ("Guess the word!")
+print ("{} {}".format("Guess the word! The word is:", newStr))
 print (newStr)
 start = time.time()
 found = True
 while (found):
-	inStr = raw_input("Enter a word (Write 'Lemme cheat' to cheat): ")
+	inStr = raw_input("Enter a word: ")
 	if(inStr == str):
 		end = time.time()
 		print("You found it!")
